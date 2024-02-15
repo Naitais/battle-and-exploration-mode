3f23bd02284e6c999ec30898bfaf54ec997a98d9
Naitais <enzo.f9849@gmail.com>
2024-02-15 13:20:07 -0300

    added indicator on round controller bar for the playing entity

--- scripts/round_bar_state.gd
+++ scripts/round_bar_state.gd
@@ -11,14 +11,23 @@ func _ready():
 	set_physics_process(false)
 
 func start_round_entities_bar_controller():
-	#sets columns with amount of entities in the round
+	#sets columns with amount of entities in the round and selects the current entity playing changing colour
 	if GlobalVar.entities_in_combat.size() > 0:
 		if entities_container.get_children().size() > 0:
-			entities_container.get_child(GlobalVar.entity_index).self_modulate = Color(137,137,255,110)
+			
+			for child in entities_container.get_children():
+				if entities_container.get_children().find(child) == GlobalVar.entity_index:
+					child.self_modulate = Color(16,8,2)
+					
+				else: 
+					#if it is not their turn
+					child.self_modulate = Color(1,1,1)
+					
+		#wait until entities in combat list is filled
 		await get_tree().create_timer(1).timeout
 		entities_container.columns = GlobalVar.entities_in_combat.size()
-	#creates and adds a label with the name of the entity 
-	#later I should replace labels with texture rects containing pictures of entities
+		
+		#creates and adds a container with texture of entity
 		if entity_bar_empty:
 			for entity in GlobalVar.entities_in_combat:
 				var entity_portrait: PanelContainer = entity.get_node("PortraitContainer").duplicate()
@@ -37,5 +46,4 @@ func _exit_state() -> void:
 	
 func _physics_process(_delta):
 	start_round_entities_bar_controller()
-	print(GlobalVar.entity_index)
 	
